5bdecc228c56448c986b6df0f31eab75d192ca36
kali <kali@example.com>
2026-08-08 17:31:33 -0300

    Calculando o Raio

---
+++ Intro em C/area_piscina.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    //raio da piscina
+    double raio = 2;
+    // área da Piscina
+    double areap; 
+    areap = M_PI * raio * raio;
+    printf("Área: %f\n", areap);
+
+
+}
