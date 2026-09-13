d4ddcb3556caa9cdd2fa34bd1014196c87fe2b7b
OdaGab <odagab@example.com>
2026-09-13 02:24:08 -0300

    Estudando C exercicio do livro Deitel

--- Exercicio317.c
+++ Exercicio317.c
@@ -1,7 +1,47 @@
+/*
+No livro a média está errada 
+Livro: Como programar em C 
+H.M Deitel e P.J Deitel 
+Segunda Edição página 77
+*/
+
+
 #include <stdio.h>
 
-main()
+int main()
 {
-    printf("Entre com os litros consumidos (-1 para finalizar):");
 
+    float litros, km, taxa, txtotal = 0;
+    int fim = 0, cont = 0;
+
+    while (litros != -1 && km != -1)
+    {
+        printf("Entre com os litros consumidos (-1 para finalizar):");
+        scanf("%f", &litros);
+
+        if(litros == -1){
+            printf("Obrigado por utilizar este software.\n");
+            break;
+        }
+
+        printf("Entre com os Km percorridos:");
+        scanf("%f", &km);
+
+        if(km == -1){
+        printf("Obrigado por utilizar este software.\n");
+        break;
+        }
+
+        taxa = km / litros;
+
+        printf("A taxa km/litro para esse tanque foi: %f \n", taxa);
+
+        txtotal = txtotal + taxa;
+
+        cont = cont + 1;
+  
+    }  
+
+    
+    printf("A taxa total de km/litro por foi: %f \n", txtotal/cont);
 }
